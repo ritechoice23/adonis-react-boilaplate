7d65aadad1ef8df97d61580a9c5b7af739a711bb
ritechoice23 <daramolatunde23@gmail.om>
2024-12-29 16:06:22 +0100

    shadcn

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Head } from '@inertiajs/react'
-import ApplicationLogo from './components/ApplicationLogo'
+import ApplicationLogo from '@/components/ApplicationLogo';
 
 export default function Home() {
   return (
