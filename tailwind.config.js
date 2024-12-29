7b527fc33a207ca027e03c424c9b7795550803ee
ritechoice23 <daramolatunde23@gmail.om>
2024-12-29 15:57:33 +0100

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.edge",
+    "./inertia/**/*.{tsx,jsx,js,ts}",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
