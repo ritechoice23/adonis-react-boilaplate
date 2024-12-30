ef788f4e58d24c92b09ef649bb4916cf38812c88
ritechoice23 <daramolatunde23@gmail.om>
2024-12-30 18:34:40 +0100

    add scheduler

--- adonisrc.ts
+++ adonisrc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('@izzyjs/route/commands'), () => import('@adonisjs/mail/commands'), () => import('adonisjs-jobs/commands')],
+  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('@izzyjs/route/commands'), () => import('@adonisjs/mail/commands'), () => import('adonisjs-jobs/commands'), () => import('adonisjs-scheduler/commands')],
 
   /*
   |--------------------------------------------------------------------------
@@ -40,7 +40,11 @@ export default defineConfig({
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@izzyjs/route/izzy_provider'),
     () => import('@adonisjs/mail/mail_provider'),
-    () => import('adonisjs-jobs/jobs_provider')
+    () => import('adonisjs-jobs/jobs_provider'),
+    {
+      file: () => import('adonisjs-scheduler/scheduler_provider'),
+      environment: ['console'],
+    }
   ],
 
   /*
@@ -51,7 +55,10 @@ export default defineConfig({
   | List of modules to import before starting the application.
   |
   */
-  preloads: [() => import('#start/routes'), () => import('#start/kernel')],
+  preloads: [() => import('#start/routes'), () => import('#start/kernel'), {
+    file: () => import('#start/scheduler'),
+    environment: ['console'],
+  }],
 
   /*
   |--------------------------------------------------------------------------
